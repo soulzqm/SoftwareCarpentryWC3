1d7385a3dd5d55246b17bb6392a74ce851ca77fa
soulzqm <47166427+soulzqm@users.noreply.github.com>
2019-02-18 06:42:13 +0800

    Update mundaneMath.py

--- mundaneMath.py
+++ mundaneMath.py
@@ -4,6 +4,8 @@ Created on Thu Feb 14 03:14:50 2019
 
 @author: qimin
 """
-
-a = sum(x for x in range(0,101,2))
-print(a)
+"""
+start from 0 with step size of 2, and end at 100, add the number 0,2,4,....100 together by "sum"
+"""
+tol = sum(x for x in range(0,101,2))
+print(tol)
